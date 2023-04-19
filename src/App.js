590ee1c8e489529e48744fe5d7b08d5ab33c41f3
Sai Sawant <saisawant2003@gmail.com>
2023-04-19 15:52:24 +0530

    react basic configuration done

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {Routes,Route}from "react-router-dom"
+
+import Layout from "./components/Layout"
+import Public from './components/Public';
+import Login from "./features/auth/Login"
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList"
+import UsersList from "./features/users/UsersList"
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Public/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="dash" element={<DashLayout/>}>
+            <Route index element={<Welcome/>}/>
+            <Route path="notes" element={<NotesList/>}/>
+            <Route path="users" element={<UsersList/>}/>
+
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
